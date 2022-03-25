6dabb93077d0bedac474fb42ebeb82073471aa1c
TaniaVF <102386151+TaniaVF@users.noreply.github.com>
2022-03-25 13:42:28 -0600

    Add files via upload
    Archivo finalizado: numeros divisores

---
+++ divisores.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "introduce un numero"
+read a
+n=$a
+echo "Los divisores de ese numero son"
+while [ $n -ge 1 ]
+do
+d=$(echo " scale=0; $a%$n" | bc -l)
+if [ $d -eq 0 ]
+then 
+echo $n
+fi
+n=$(echo "$n-1" | bc -l)
+done
